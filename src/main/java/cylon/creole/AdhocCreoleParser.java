83ad61d4ac90f52b4cbdc038916d94519cd1b227
Eungju PARK <eungju at gmail dot com>
2011-05-14 13:33:35 +0900

    Support Creole 1.0 link syntax except free-standing URLs.

--- src/main/java/cylon/creole/AdhocCreoleParser.java
+++ src/main/java/cylon/creole/AdhocCreoleParser.java
@@ -234,18 +234,18 @@ public class AdhocCreoleParser {
 	}
 
 	static class LinkRule extends InlineRule {
-		static final String REGEX = "\\[\\[(.+?)\\s*(?:\\|\\s*(.+?))?\\]\\]";
+		static final String REGEX = "\\[\\[(.+?)(?:\\|(.+?))?\\]\\]";
 		
 		public LinkRule() {
 			super(REGEX);
 		}
 		public void matched(String[] group, AdhocCreoleParser parser) {
 			TextComposite parent = parser.cursor.ascendUntil(TextComposite.class);
-			Link node = new Link(group[1]);
+			Link node = new Link(group[1].trim());
 			parent.addChild(node);
 			parser.cursor.descend(node);
 			if (group[2] != null) {
-				parser.parseInline(group[2]);
+				parser.parseInline(group[2].trim());
 			}
 			parser.cursor.ascendTo(node);
 		}
---
+++ src/test/java/cylon/creole/LinkTest.java
@@ -0,0 +1,48 @@
+package cylon.creole;
+
+import cylon.dom.DomBuilder;
+import org.junit.Before;
+import org.junit.Ignore;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * http://www.wikicreole.org/wiki/Creole1.0#section-Creole1.0-LinksInternalExternalAndInterwiki
+ */
+public class LinkTest extends DomBuilder {
+    private AdhocCreoleParser dut;
+
+    @Before
+    public void beforeEach() {
+        dut = new AdhocCreoleParser();
+    }
+
+    @Test public void
+    internalLink() {
+        assertEquals(document(p(link("link"))), dut.document("[[link]]"));
+        assertEquals(document(p(link("MyBigPage", t("Go to my page")))), dut.document("[[MyBigPage|Go to my page]]"));
+    }
+
+    @Test public void
+    externalLink() {
+        assertEquals(document(p(link("http://www.wikicreole.org/"))), dut.document("[[http://www.wikicreole.org/]]"));
+        assertEquals(document(p(link("http://www.wikicreole.org/", t("Visit the WikiCreole website")))), dut.document("[[http://www.wikicreole.org/|Visit the WikiCreole website]]"));
+    }
+
+    @Test public void
+    imagesInsideLinksMustBeSupported() {
+        assertEquals(document(p(link("link", image("image"), t(" inside link")))), dut.document("[[link|{{image}} inside link]]"));
+    }
+
+    @Ignore("TODO")
+    @Test public void
+    freeStandingURLsShouldBeDetectedAndTurnedIntoLinks() {
+        assertEquals(document(p(link("http://www.rawlink.org/"), t(", "), link("http://www.another.rawlink.org"))), dut.document("http://www.rawlink.org/, http://www.another.rawlink.org"));
+    }
+
+    @Test public void
+    interwikiLink() {
+        assertEquals(document(p(link("Ohana:WikiFamily"))), dut.document("[[Ohana:WikiFamily]]"));
+    }
+}
