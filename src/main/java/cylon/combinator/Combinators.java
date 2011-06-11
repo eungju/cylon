4d9f298ff4deb3439a6705b4239fedece62d8f3e
Eungju PARK <eungju at gmail dot com>
2011-06-11 17:02:43 +0900

    And and not predicates.

--- src/main/java/cylon/combinator/Combinators.java
+++ src/main/java/cylon/combinator/Combinators.java
@@ -20,4 +20,28 @@ public class Combinators {
     public static ActionParser optional(Parser expression) {
         return new ChoiceCombinator(expression, new EmptyParser());
     }
+
+    public static ActionParser and(final Parser expression) {
+        return new ActionParser() {
+            public Result parse(CharSequence input) {
+                Result result = expression.parse(input);
+                if (result.isSuccess()) {
+                    return Result.success(action.invoke(input.subSequence(0, 0)), input);
+                }
+                return Result.failure(input);
+            }
+        };
+    }
+
+    public static Parser not(final Parser expression) {
+        return new ActionParser() {
+            public Result parse(CharSequence input) {
+                Result result = expression.parse(input);
+                if (result.isFailure()) {
+                    return Result.success(action.invoke(input.subSequence(0, 0)), input);
+                }
+                return Result.failure(input);
+            }
+        };
+    }
 }
--- src/test/java/cylon/combinator/CombinatorsTest.java
+++ src/test/java/cylon/combinator/CombinatorsTest.java
@@ -11,37 +11,37 @@ import static org.junit.Assert.*;
 
 public class CombinatorsTest {
     @Test public void
-    sequenceSuccess() {
+    sequenceOperatorSuccess() {
         assertThat(Combinators.sequence(regex("a"), regex("b")).parse("abc"), is(Result.success(Arrays.asList("a", "b"), "c")));
     }
 
     @Test public void
-    sequenceFailure() {
+    sequenceOperatorFailure() {
         assertThat(Combinators.sequence(regex("a"), regex("c")).parse("abc"), is(Result.failure("abc")));
     }
 
     @Test public void
-    choiceSuccess() {
+    choiceOperatorSuccess() {
         Parser dut = Combinators.choice(regex("a"), regex("."));
         assertThat(dut.parse("abc"), is(Result.success("a", "bc")));
         assertThat(dut.parse("123"), is(Result.success("1", "23")));
     }
 
     @Test public void
-    choiceFailure() {
+    choiceOperatorFailure() {
         Parser dut = Combinators.choice(regex("a"), regex("1"));
         assertThat(dut.parse("!@#"), is(Result.failure("!@#")));
     }
 
     @Test public void
-    zeroOrMore() {
+    zeroOrMoreOperator() {
         Parser dut = Combinators.zeroOrMore(regex("a"));
         assertThat(dut.parse("bbcc"), is(Result.success(new ArrayList<CharSequence>(), "bbcc")));
         assertThat(dut.parse("aabbcc"), is(Result.success(Arrays.asList("a", "a"), "bbcc")));
     }
 
     @Test public void
-    oneOrMore() {
+    oneOrMoreOperator() {
         Parser dut = Combinators.oneOrMore(regex("a"));
         assertThat(dut.parse("abbcc"), is(Result.success(Arrays.asList("a"), "bbcc")));
         assertThat(dut.parse("aabbcc"), is(Result.success(Arrays.asList("a", "a"), "bbcc")));
@@ -49,9 +49,23 @@ public class CombinatorsTest {
     }
 
     @Test public void
-    optional() {
+    optionalOperator() {
         Parser dut = Combinators.optional(regex("a"));
         assertThat(dut.parse("bc"), is(Result.success("", "bc")));
         assertThat(dut.parse("abc"), is(Result.success("a", "bc")));
+    }
+
+    @Test public void
+    andPredicate() {
+        Parser dut = Combinators.and(regex("a"));
+        assertThat(dut.parse("abc"), is(Result.success("", "abc")));
+        assertThat(dut.parse("bc"), is(Result.failure("bc")));
+    }
+
+    @Test public void
+    notPredicate() {
+        Parser dut = Combinators.not(regex("a"));
+        assertThat(dut.parse("bc"), is(Result.success("", "bc")));
+        assertThat(dut.parse("abc"), is(Result.failure("abc")));
     }
 }
